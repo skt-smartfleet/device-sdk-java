98794ddb77f53116bc1f566dca6d88f35cae0463
kown <skillove@gmail.com>
2017-12-04 14:40:48 +0900

    주석

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import com.sktelecom.smartfleet.sdk.net.MqttWrapper;
-import com.sktelecom.smartfleet.sdk.net.RPCType;
 import org.apache.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
@@ -10,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    MqttWrapper mqtt = MqttWrapper.getInstance();
+	    // MQTT메세지 응답 리스너
         mqtt.setListener(new MqttWrapper.MqttWrapperListener() {
             @Override
             public void onMqttConnected() {
@@ -22,44 +22,38 @@ public class Main {
             }
 
             @Override
+            /**
+             * RPC 메세지 수신
+             * Response응답은 SDK 에서 처리되고 아래 함수내에서 요청을 조건 구현후 조건에 맞는 Result 함수를호출하도록한다.
+             */
             public void onRPCMessageArrived(String topic, String request_id, String method, MqttMessage mqttMessage) {
-                logger.info("onRPCMessageArrived..");
-                logger.info("topic : "+topic);
-                logger.info("method : "+method);
-                logger.info("request_id : "+request_id);
-
                 if (method.equals(DEVICE_ACTIVATION)) {
+                    // 단말이 Activation이 필요한 경우에 Activation Flow에 따라 정상적으로 접속이 되는지 확인
                     mqtt.resultDeviceActivation("00가0000",topic);
                 } else if (method.equals(FIRMWARE_UPDATE)) {
+                    // F/W Update에 대한 원격 요청을 정상적으로 수행하는지 확인
                     mqtt.resultFirmwareUpdate(topic);
                 } else if (method.equals(OBD_RESET)) {
+                    // 단말 리셋을 정상적으로 수행하는지 확인
                     mqtt.resultOBDReset(topic);
                 } else if (method.equals(DEVICE_SERIAL_NUMBER_CHECK)) {
+                    // 단말 시리얼키 검사
                     mqtt.resultDeviceSerialNumberCheck("70d71b00-71c9-11e7-b3e0-e5673983c7b9",topic);
                 } else if (method.equals(CLEAR_DEVICE_DATA)) {
+                    // 단말 데이터초기화
                     mqtt.resultClearDeviceData(topic);
                 } else if (method.equals(FIRMWARE_UPDATE_CHUNK)) {
+                    // Firmware Update Chunk 이벤트
                     mqtt.resultFirmwareUpdateChunk(topic);
                 }
             }
         });
-
+        // MQTT서버 연결주소 설정
         mqtt.setHost("localhost");
         mqtt.setPort("8443");
+        // 사용자 인증키(20자리)
         mqtt.setToken("00000000000000000001");
-
+        // MQTT서버연결(ssl)
         mqtt.TRE_Connect();
-
-//        mqtt.TRE_SendTrip();
-//        mqtt.TRE_SendMicroTrip();
-//        mqtt.TRE_SendHfd();
-//        mqtt.TRE_SendDiagInfo();
-//        mqtt.TRE_SendDrivingCollisionWarning();
-//        mqtt.TRE_SendParkingCollisionWarning();
-//        mqtt.TRE_SendBatteryWarning();
-//        mqtt.TRE_SendUnpluggedWarning();
-//        mqtt.TRE_SendTurnOffWarning();
-//
-//        System.exit(-1);
     }
 }
